2eb19b957797de3e11151a8e2fc2a917d4b2ee3f
Yujin Chung <yujin.mar.chung@gmail.com>
2020-03-12 15:18:59 +0900

    refactored,

--- Part3/rude_words.py
+++ Part3/rude_words.py
@@ -1,14 +1,27 @@
-# rude words detector v0.1
+# rude words detector v0.2
 
 rude_words = ['idiot', 'butt', 'devil', 'heck', 'jerk', 'crap', 'darn']
 
-with open("sometext.txt") as my_story:
-    contents = my_story.read()
-    rude_count = 0
-    for rude in rude_words:
-        if rude in contents:
+def check_line(line):
+    rude_count = 0 
+    words = line.split(" ")  # list of words 
+    for word in words:
+        if word in rude_words:
             rude_count += 1
-            print(f"Found rude word: {rude}")
-if rude_count == 0:
-    print("Congrats, no rude word!")
+            print(f"Foudn rude word: {word}")
+    return rude_count
+
+def check_file(filename):
+        with open(filename) as my_story:
+            rude_count = 0
+            for line in my_story:
+                rude_count += check_line(line)
+        if rude_count == 0:
+            print("Congrats, no rude word!")
+        else:
+            print("You should take another look to your writing! ")    
+        
+if __name__ == "__main__":
+    check_file("sometext.txt")
+
     
